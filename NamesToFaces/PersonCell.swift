d79bc8fd27a19a1e2d0271b317c0d009e4f85014
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-10-28 19:59:17 -0300

    feat: Add basic collection and cell structure

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Rodrigo on 28-10-24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
